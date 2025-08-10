d9b3a129ad6f413851720d98ee7bb7ea64382509
daksharma3163 <9923103163@mail.jiit.ac.in>
2025-08-10 15:29:20 +0530

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,10 @@
+with open("content.txt",'r') as file:
+    content = file.read()
+    char_count = {}
+    for char in content:
+        char_count[char] = char_count.get(char, 0) + 1
+
+    print(f"Total characters: {len(content)}")
+    print("Character occurrences:")
+    for char, count in char_count.items():
+        print(f"'{char}': {count}")
